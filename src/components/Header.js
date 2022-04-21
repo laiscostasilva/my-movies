85571612c827ace46ef385e3012f5e230ba65195
Lais Costa <laiscosta@MacBook-Air-de-Lais.local>
2022-04-21 18:41:45 -0300

    arq-frontend-unidade1-10

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { MainMenu } from "./MainMenu";
+
+//arrow function que retorna um conteúdo
+export const Header = () => (
+    <header>
+        <h1>My Movies</h1>
+
+        <MainMenu />
+    </header>
+);
